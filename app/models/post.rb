3009412d2028a682d27dca46fb9db935f602614d
Mollie Dickenson <molliedickenson@gmail.com>
2021-07-01 17:16:14 -0400

    commit tree 5a0d66ff942830ee21511704c00b6fe6ebbfa331

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+    validates :title, presence: true 
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"] }
+    validate :clickbait
+
+    def clickbait
+        words = ["Won't Believe", "Secret", "Top [number]", "Guess"]
+        if title
+            good_titles = words.any? {|w| title.include?(w)}
+            if !good_titles
+                errors.add(:title, "Not clickbait-y enough")
+            end
+        end
+    end
 end
